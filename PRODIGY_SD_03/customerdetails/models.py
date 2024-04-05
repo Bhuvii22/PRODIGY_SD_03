d97b9d22e7de42d455873c01b6a36ee34bc8c977
Bhuvii22 <67328852+Bhuvii22@users.noreply.github.com>
2024-04-05 11:14:36 +0530

    Add files via upload

---
+++ PRODIGY_SD_03/customerdetails/models.py
@@ -0,0 +1,13 @@
+
+# Create your models here.
+from django.db import models  
+class Employee(models.Model):  
+    cid = models.CharField(max_length=20)  
+    cname = models.CharField(max_length=100)  
+    cemail = models.EmailField()  
+    ccontact = models.IntegerField(max_length=15) 
+
+    def __str__(self):
+        return "%s " %(self.cname) 
+    class Meta:  
+        db_table = "customer"
---
+++ PRODIGY_SD_03/customerdetails/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from django.shortcuts import render, redirect  
+from customerdetails.forms import EmployeeForm  
+from customerdetails.models import Employee  
+ 
+def emp(request):  
+    if request.method == "POST":  
+        form = EmployeeForm(request.POST)  
+        if form.is_valid():  
+            try:  
+                form.save()  
+                return redirect('show')  
+            except:  
+                pass  
+    else:  
+        form = EmployeeForm()  
+    return render(request,'index.html',{'form':form})  
+def show(request):  
+    employees = Employee.objects.all()  
+    return render(request,"show.html",{'employees':employees})  
+def edit(request, id):  
+    print("value of id",id)
+    employee = Employee.objects.get(id=id)  
+    form=EmployeeForm()
+    return render(request,'edit.html', {'employee':employee,'form':form})  
+def update(request, id):  
+    employee = Employee.objects.get(id=id)  
+    form = EmployeeForm(request.POST, instance = employee)  
+    if form.is_valid():  
+        form.save()  
+        return redirect("/show")  
+    return render(request, 'edit.html', {'employee': employee,'form':form})  
+   
+def destroy(request, id): 
+    employee = Employee.objects.get(id= id)  
+    employee.delete()  
+    return redirect("/show")
